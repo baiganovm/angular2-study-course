623914abd4c21e0febcb6bcdae72debbeb975ef4
Mikhail Baiganov <baiganovm@gmail.com>
2017-11-28 01:32:21 +0400

    Basic Service

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 const DISH = {
   name: 'Uthappizza',
@@ -49,6 +49,7 @@ const DISH = {
 })
 export class DishdetailComponent implements OnInit {
 
+  @Input()
   dish = DISH;
 
   constructor() { }
--- src/app/shared/dishes.ts
+++ src/app/shared/dishes.ts
@@ -7,38 +7,37 @@ export const DISHES: Dish[] = [
     category: 'mains',
     label: 'Hot',
     price: '4.99',
-    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ' +
-    'ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
     comments: [
       {
         rating: 5,
-        comment: 'Imagine all the eatables, living in conFusion!',
-        author: 'John Lemon',
-        date: '2012-10-16T17:57:28.556094Z'
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
       },
       {
         rating: 4,
-        comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
-        author: 'Paul McVites',
-        date: '2014-09-05T17:57:28.556094Z'
+        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
       },
       {
         rating: 3,
-        comment: 'Eat it, just eat it!',
-        author: 'Michael Jaikishan',
-        date: '2015-02-13T17:57:28.556094Z'
+        comment: "Eat it, just eat it!",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
       },
       {
         rating: 4,
-        comment: 'Ultimate, Reaching for the stars!',
-        author: 'Ringo Starry',
-        date: '2013-12-02T17:57:28.556094Z'
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
       },
       {
         rating: 2,
-        comment: 'It\'s your birthday, we\'re gonna party!',
-        author: '25 Cent',
-        date: '2011-12-02T17:57:28.556094Z'
+        comment: "It's your birthday, we're gonna party!",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
       }
     ]
   },
@@ -52,33 +51,33 @@ export const DISHES: Dish[] = [
     comments: [
       {
         rating: 5,
-        comment: 'Imagine all the eatables, living in conFusion!',
-        author: 'John Lemon',
-        date: '2012-10-16T17:57:28.556094Z'
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
       },
       {
         rating: 4,
-        comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
-        author: 'Paul McVites',
-        date: '2014-09-05T17:57:28.556094Z'
+        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
       },
       {
         rating: 3,
-        comment: 'Eat it, just eat it!',
-        author: 'Michael Jaikishan',
-        date: '2015-02-13T17:57:28.556094Z'
+        comment: "Eat it, just eat it!",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
       },
       {
         rating: 4,
-        comment: 'Ultimate, Reaching for the stars!',
-        author: 'Ringo Starry',
-        date: '2013-12-02T17:57:28.556094Z'
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
       },
       {
         rating: 2,
-        comment: 'It\'s your birthday, we\'re gonna party!',
-        author: '25 Cent',
-        date: '2011-12-02T17:57:28.556094Z'
+        comment: "It's your birthday, we're gonna party!",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
       }
     ]
   },
@@ -92,33 +91,33 @@ export const DISHES: Dish[] = [
     comments: [
       {
         rating: 5,
-        comment: 'Imagine all the eatables, living in conFusion!',
-        author: 'John Lemon',
-        date: '2012-10-16T17:57:28.556094Z'
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
       },
       {
         rating: 4,
-        comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
-        author: 'Paul McVites',
-        date: '2014-09-05T17:57:28.556094Z'
+        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
       },
       {
         rating: 3,
-        comment: 'Eat it, just eat it!',
-        author: 'Michael Jaikishan',
-        date: '2015-02-13T17:57:28.556094Z'
+        comment: "Eat it, just eat it!",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
       },
       {
         rating: 4,
-        comment: 'Ultimate, Reaching for the stars!',
-        author: 'Ringo Starry',
-        date: '2013-12-02T17:57:28.556094Z'
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
       },
       {
         rating: 2,
-        comment: 'It\'s your birthday, we\'re gonna party!',
-        author: '25 Cent',
-        date: '2011-12-02T17:57:28.556094Z'
+        comment: "It's your birthday, we're gonna party!",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
       }
     ]
   },
@@ -132,33 +131,33 @@ export const DISHES: Dish[] = [
     comments: [
       {
         rating: 5,
-        comment: 'Imagine all the eatables, living in conFusion!',
-        author: 'John Lemon',
-        date: '2012-10-16T17:57:28.556094Z'
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
       },
       {
         rating: 4,
-        comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
-        author: 'Paul McVites',
-        date: '2014-09-05T17:57:28.556094Z'
+        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
       },
       {
         rating: 3,
-        comment: 'Eat it, just eat it!',
-        author: 'Michael Jaikishan',
-        date: '2015-02-13T17:57:28.556094Z'
+        comment: "Eat it, just eat it!",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
       },
       {
         rating: 4,
-        comment: 'Ultimate, Reaching for the stars!',
-        author: 'Ringo Starry',
-        date: '2013-12-02T17:57:28.556094Z'
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
       },
       {
         rating: 2,
-        comment: 'It\'s your birthday, we\'re gonna party!',
-        author: '25 Cent',
-        date: '2011-12-02T17:57:28.556094Z'
+        comment: "It's your birthday, we're gonna party!",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
       }
     ]
   }
